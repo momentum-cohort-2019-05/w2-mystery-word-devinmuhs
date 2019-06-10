30992834d3382427ec7d707c9b85acc35cfe2a45
Devin Muhs <devinmuhs@gmail.com>
2019-06-10 09:45:57 -0400

    it works

--- mystery_word.py
+++ mystery_word.py
@@ -1,23 +1,23 @@
-import random
-f = open("words.txt")
-all_words = (f.read()).upper()
+def guess_taker():
+    import random
+    f = open("words.txt")
+    all_words = (f.read()).upper()
 
 
-split_all_words = all_words.split()
+    split_all_words = all_words.split()
 
-easy_words = []
-normal_words = []
-hard_words = []
+    easy_words = []
+    normal_words = []
+    hard_words = []
+    for word in split_all_words:
+        if len(word) >= 8:
+            hard_words.append(word)
+        elif len(word) >= 6 and len(word) <= 8:
+            normal_words.append(word)
+        elif len(word) <= 6 and len(word) >= 4:
+            easy_words.append(word)
 
-for word in split_all_words:
-    if len(word) >= 8:
-        hard_words.append(word)
-    elif len(word) >= 6 and len(word) <= 8:
-        normal_words.append(word)
-    elif len(word) <= 6 and len(word) >= 4:
-        easy_words.append(word)
 
-def guess_taker(input):
     difficulty_selector = input("Welcome to the Myster Word Game!\n\nPlease enter your difficulty level:\n\nEASY\nNORMAL\nHARD\n")
     upper_case_difficulty = difficulty_selector.upper()
 
@@ -41,63 +41,56 @@ def guess_taker(input):
     current_guesses= []
 
     def display_letter(letter, guesses):
-        """
-        Conditionally display a letter. If the letter is already in
-        the list `guesses`, then return it. Otherwise, return "_".
-        """
         if letter in guesses:
             return letter
         else:
             return "_"
 
-    # [display_letter(letter, current_guesses)
-    # for letter in word]
-
     def print_word(word, guesses):
         output_letters = []
         for letter in word:
             output_letters.append(display_letter(letter, guesses))
-        print(" ".join(output_letters))
-
+        return (" ".join(output_letters))
         
     guess_counter = 8
-    print_word(word, current_guesses)
+    print(word_match)
     print("Lives remaining: ", guess_counter)
     guess = input("Please input your letter guess: ").upper()
 
-    # while len(guess) != 1 and guess_counter > 0:
-    #     print("ERROR! Please enter one letter.\n")
-    #     guess = input("Please input your letter guess: ").upper()
-    #     guess_counter -= 1
-    #     print("Lives remaining: ", guess_counter)
+    
+    wordGuessed = False
 
-    while guess_counter > 0:
+    while guess_counter > 0 and wordGuessed is False:
         if len(guess) != 1:
             print("ERROR! Please enter one letter.")
-            # print("Lives remaining: ", guess_counter)
-        if guess in word and len(guess) == 1:
+        if guess in word and len(guess) == 1 and guess not in current_guesses:
             print("Correct!", guess, "is in the word!")
+            if guess not in current_guesses and len(guess) == 1:
+                current_guesses.append(guess)
+                if word_match == print_word(word, current_guesses):
+                    wordGuessed is True
+                    print(print_word(word, current_guesses))
+                    print("Congrats! You beat the game! The word was", word)
+                    break
         if guess not in word and guess not in current_guesses:
             print("Sorry,", guess, "is not in the word.")
             guess_counter -= 1
-            # print("Lives remaining: ", guess_counter)
         if guess in current_guesses:
             print("INVALID ENTRY! Already been guessed!")
-            # print("Lives remaining: ", guess_counter)
-        if guess not in current_guesses and len(guess) == 1:
-            current_guesses.append(guess)
         if guess_counter > 0:
-            print_word(word, current_guesses)
+            print(print_word(word, current_guesses))
             print("Current guesses: ", current_guesses)
             print("Lives remaining: ", guess_counter, "\n")
             guess = input("Please input your letter guess:").upper()
-    print("GAME OVER! The word was", word.upper())
+
+    if guess_counter == 0:
+        print("GAME OVER! The word was", word.upper())
     end_game = input("Play again? Type Yes or No. ")
     end_game = end_game.lower()
 
     if end_game == "yes":
-        guess_taker(input)
+        guess_taker()
     else:
         print("Good-bye!")
 
-guess_taker(input)
+guess_taker()
